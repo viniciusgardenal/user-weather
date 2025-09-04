c0dd1193b654266058e95c076beb5f4ee36afd10
Vinícius Gardenal <vinicius_sgardenal@hotmail.com>
2025-09-04 13:24:38 -0300

    Reapply "main"
    This reverts commit 0d96fe51154da1c7da07454392a0da3df7e1a291.

---
+++ frontend/src/App.jsx
@@ -0,0 +1,26 @@
+// src/App.jsx
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './components/Header.jsx';
+import Home from './pages/Home.jsx';
+import CreateUser from './pages/CreateUser.jsx';
+import UserDetails from './pages/UserDetails.jsx';
+import Weather from './pages/Weather.jsx';
+
+function App() {
+  return (
+    <Router>
+      <Header />
+      <main className="container">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/users/new" element={<CreateUser />} />
+          <Route path="/users/:id" element={<UserDetails />} />
+          <Route path="/weather" element={<Weather />} />
+        </Routes>
+      </main>
+    </Router>
+  );
+}
+
+export default App;
---
+++ frontend/src/pages/UserDetails.jsx
@@ -0,0 +1,48 @@
+// src/pages/UserDetails.jsx
+import React, { useState, useEffect } from 'react';
+import { useParams, Link } from 'react-router-dom';
+import api from '../services/api';
+
+const UserDetails = () => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const { id } = useParams();
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      setLoading(true);
+      try {
+        const response = await api.get(`/users/${id}`);
+        setUser(response.data);
+      } catch (error) {
+        console.error("Usuário não encontrado:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchUser();
+  }, [id]);
+
+  if (loading) return <p>Carregando detalhes do usuário...</p>;
+  if (!user) return (
+    <div>
+        <p>Usuário não encontrado.</p>
+        <Link to="/">Voltar para a lista</Link>
+    </div>
+  );
+
+  return (
+    <div>
+      <h2>Detalhes do Usuário</h2>
+      <div className="user-details">
+        <p><strong>ID:</strong> {user.id}</p>
+        <p><strong>Nome:</strong> {user.name}</p>
+        <p><strong>Email:</strong> {user.email}</p>
+      </div>
+      <br />
+      <Link to="/">Voltar para a lista</Link>
+    </div>
+  );
+};
+
+export default UserDetails;
